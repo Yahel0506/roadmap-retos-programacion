b7f74331b41d1a81bea082789664a4250d2039b6
Yahel0506 <yahelloppez@gmail.com>
2025-04-16 09:42:43 -0600

    yahel0506.kt

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/yahel0506.kt
@@ -0,0 +1,16 @@
+fun main() {
+  // kotlinlang.org
+  /* Reto 00:
+  Sintaxis, variables, tipos de datos y hola mundo */
+
+  val usuario: String = "Angel Yahel"
+  var caracter_favorito: Char = '/'
+  var edad: Int = 14
+  val dinero_soñado: Long = 2918431924389721L
+  var dinero_actual: Float = 650.235F
+  val pi: Double = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461
+  val nacionalidad_mexicana: Boolean = true
+  val frase_para_imprimir: String = "Hola, Kotlin!"
+
+  println(frase_para_imprimir)
+}
